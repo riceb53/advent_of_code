3b217eb2ff04f9ecc07d65df449318f78508b306
brian <riceb53@gmail.com>
2022-12-21 21:32:54 -0800

    day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,19 +1,22 @@
 # part one
-
 f = open('day1input.txt', 'r')
 food = f.read().split("\n")
 # print(food)
 f.close()
 
 
-max_food = 0
+top_three_food_carriers = [0,0,0]
 current_elf_food = 0
 for item in food:
     if item == '':
-        if current_elf_food > max_food:
-            max_food = current_elf_food
+        if current_elf_food > top_three_food_carriers[-1]:
+            for idx, food_carrier in enumerate(top_three_food_carriers):
+                if current_elf_food > food_carrier:
+                    top_three_food_carriers.insert(idx, current_elf_food)
+                    top_three_food_carriers.pop()
+                    break            
         current_elf_food = 0
     else:        
         current_elf_food += int(item)
-print(max_food)
+print(sum(top_three_food_carriers))
 
